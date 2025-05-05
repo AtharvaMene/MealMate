e31bf79442a01c25ad2dff1d093f6ea2b03bbbb2
Atharva Mene <atharvamene@gmail.com>
2025-05-06 02:23:40 +0530

    updates

--- app/src/main/java/com/example/mealmate/MainActivity.java
+++ app/src/main/java/com/example/mealmate/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.mealmate;
 
-import android.content.Intent;
+ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
